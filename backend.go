6292341c0a08c893412caef79db906eed0063433
autom8ter <admin@autom8ter.com>
2019-04-30 19:41:35 -0600

    update

--- backend.go
+++ backend.go
@@ -1,28 +1,67 @@
-package backend
+package main
 
 import (
-	"github.com/autom8ter/engine"
-	"github.com/autom8ter/engine/driver"
+	"context"
+	"github.com/autom8ter/api"
+	"github.com/autom8ter/api/common"
+	"github.com/autom8ter/fire"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	"os"
 )
 
-type Backend struct {
-	plugins []driver.PluginFunc
-}
+var firedb *fire.Client
+var addr string
 
-func NewBackend(plugs ...driver.PluginFunc) *Backend {
-	return &Backend{
-		plugs,
+func main() {
+	ctx := context.TODO()
+	projectID := os.Getenv("GCLOUD_PROJECT")
+	if projectID == "" {
+		log.Fatalf("Set Firebase project ID via GCLOUD_PROJECT env variable.")
 	}
-}
 
-func (b *Backend) Serve(addr string, debug bool) error {
-	return engine.Serve(addr, debug, b.asPlugins()...)
-}
-
-func (b *Backend) asPlugins() []driver.Plugin {
-	plugs := []driver.Plugin{}
-	for _, v := range b.plugins {
-		plugs = append(plugs, v)
+	firedb, err := fire.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Cannot create client: %v", err)
 	}
-	return plugs
+	defer firedb.Fire.Close()
+	var db = &api.Database{
+		GetUserFunc: func(ctx context.Context, email *common.Identifier) (user *api.User, e error) {
+			return nil, status.Error(codes.Unimplemented, "unimplemented")
+
+		},
+		DeleteUserFunc: func(ctx context.Context, email *common.Identifier) (empty *common.Empty, e error) {
+			return nil, status.Error(codes.Unimplemented, "unimplemented")
+
+		},
+		UpdateUserFunc: func(ctx context.Context, u *api.UpdateUserRequest) (user *api.User, e error) {
+			return nil, status.Error(codes.Unimplemented, "unimplemented")
+
+		},
+		CreateUserFunc: func(ctx context.Context, u *api.User) (user *api.User, e error) {
+			return nil, status.Error(codes.Unimplemented, "unimplemented")
+		},
+		ListUsersFunc: func(e *common.Empty, stream api.DBService_ListUsersServer) error {
+			return nil
+		},
+	}
+	db.PluginFunc = func(s *grpc.Server) {
+		api.RegisterDBServiceServer(s, db)
+	}
+	var deb = &api.Debugger{
+		DebugFunc: func(ctx context.Context, s *common.String) (i *common.String, e error) {
+			return s, nil
+		},
+	}
+	deb.PluginFunc = func(s *grpc.Server) {
+		api.RegisterDebugServiceServer(s, deb)
+	}
+	if err := api.Serve(addr, db, deb); err != nil {
+		if addr == "" {
+			addr = ":3000"
+		}
+		log.Fatalln(err.Error())
+	}
 }
--- go.mod
+++ go.mod
@@ -1,20 +1,8 @@
 module github.com/autom8ter/backend
 
 require (
-	cloud.google.com/go v0.37.2
-	github.com/autom8ter/api v0.0.0-20190429084754-d13f8a797664
-	github.com/autom8ter/engine v0.0.0-20190421032226-fadad0594395
-	github.com/autom8ter/gcloud v0.0.0-20190408073117-8b7a9496ac0d
-	github.com/gorilla/schema v1.1.0 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/pkg/errors v0.8.1
-	github.com/sendgrid/rest v2.4.1+incompatible // indirect
-	github.com/sendgrid/sendgrid-go v3.4.1+incompatible
-	github.com/sfreiberg/gotwilio v0.0.0-20190413041346-4c78e78c98b6
-	github.com/spf13/cobra v0.0.3
-	github.com/stripe/stripe-go v60.9.0+incompatible
-	golang.org/x/text v0.3.2
-	google.golang.org/api v0.3.2
+	cloud.google.com/go v0.37.4
+	github.com/autom8ter/api v0.0.0-20190430235657-1baa6fe8fd11
+	github.com/autom8ter/fire v0.0.0-20190501013558-c4f159f7306d // indirect
 	google.golang.org/grpc v1.20.1
 )
